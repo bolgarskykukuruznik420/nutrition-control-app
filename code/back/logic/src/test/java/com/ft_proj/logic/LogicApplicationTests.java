4500b55b81d3e9df1e855f461cd81ac468bfc21f
Leonid Matskevich <leonidmatskevich@mail.ru>
2023-10-02 21:09:55 +0300

    logic init

---
+++ code/back/logic/src/test/java/com/ft_proj/logic/LogicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ft_proj.logic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
